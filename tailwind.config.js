47ce8cb1efb6df1c68606fcf778664c913e63ff3
hariadiarief <adi.arief77@gmail.com>
2024-05-14 13:59:17 +0700

    feat: update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,25 @@
+const { fontFamily } = require("tailwindcss/defaultTheme")
+
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
+  theme: { 
     extend: {
       colors: {
         secondary: "#F7F6DC",
         white: "#FFFEFF",
 
-      }
-    },
-  },
-  plugins: [],
-}
+        fontFamily: {
+          sans: [...fontFamily.sans],
+          heading: [...fontFamily.sans],
+        },
 
+      },
+    },
+    plugins: []
+  }
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src' // Set the alias to the root of your "src" directory
+    }
+  },
+  server: {
+    open: true, 
+    port: 3000, 
+  },
 })
